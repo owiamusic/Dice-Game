b911f6557a61457c406236629413401ed8e7bdb2
owiamusic <owiamusic@gmail.com>
2026-07-21 12:07:57 +0000

    added comments

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+// Generating Random Dice Numbers
+
 let randomNumber1 = Math.floor(Math.random() * 6 + 1);
 
 let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+//Setting the Dice Number to match the URL of the Dice Images
 
 let randomDiceImageOne = "img/dice" + +randomNumber1 + ".jpg";
 
 let randomDiceImageTwo = "img/dice" + randomNumber2 + ".jpg";
 
+// Reload Button that Refresh Page
+
 let button = document.getElementById("reloadBtn");
 button.addEventListener("click", function () {
   window.location.reload();
 });
+
+//Dice Number 1 and 2 Matching Exact Random Number
 
 let image1 = document
   .querySelectorAll("img")[0]
@@ -18,6 +26,8 @@ let image1 = document
 let image2 = document
   .querySelectorAll("img")[1]
   .setAttribute("src", randomDiceImageTwo);
+
+//Conditional that Determines what Text should be displayed to the Players.
 
 if (randomNumber1 == randomNumber2) {
   document.getElementById("winner").textContent = "Tie, Roll Dice";
